903586c1ee35b789ba99888764774e9bf8590929
Urdiales Nicolas <nurdiales@e-biz.fr>
2017-05-04 17:19:22 +0200

    {Scripts}

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#in the folder where the .war is
+#use if possible full 
+#then do :
+#     docker cp fileToDeploy.war dockerContainerName:location/to/put/fileToDeploy.war
+#ex : docker cp mywebapp.war mad_ride:/usr/local/tomcat/webapps/mywebapp.war
---
+++ start.sh
@@ -0,0 +1,1 @@
+docker run -d --name="tomcat-run" -p 8080:8080 -p 8000:8000 eclairiuss/tomcat
