9dae7a02346cf6107cd35b525284716b678a7d74
Urdiales Nicolas <nurdiales@e-biz.fr>
2017-05-04 11:26:27 +0200

    {SCRIPTS} run and insert in DB and correct init.sh

--- insertInDB.sh
+++ insertInDB.sh
@@ -1,3 +1,3 @@
-mysql -uroot -proot computer-database-db < /etc/mysql/conf.d/1-SCHEMA.sql
-mysql -uroot -proot computer-database-db < /etc/mysql/conf.d/2-PRIVILEGES.sql
-mysql -uroot -proot computer-database-db < /etc/mysql/conf.d/3-ENTRIES.sql
+mysql -uroot -proot -h 0.0.0.0 -P 3306 < 1-SCHEMA.sql
+mysql -uroot -proot -h 0.0.0.0 -P 3306 computer-database-db < 2-PRIVILEGES.sql
+mysql -uroot -proot -h 0.0.0.0 -P 3306 computer-database-db < 3-ENTRIES.sql
---
+++ start.sh
@@ -0,0 +1,1 @@
+docker run -d --name="mysql-run" -e MYSQL_PASSWORD=root -p 3306:3306 eclairiuss/mysql
