4ade866ba136191c1a1107c21d142a95942d1ed6
Urdiales Nicolas <nurdiales@e-biz.fr>
2017-05-09 11:46:41 +0200

    {UPDATE}

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 
 docker network create my_network
 
-docker run --network my_network -d --name="tomcat-run" -p 8080:8080 -p 8000:8000 eclairiuss/tomcat
-docker run --network my_network -d --name="mysql-run" -e MYSQL_PASSWORD=root -p 3306:3306 eclairiuss/mysql
+docker run --network my_network -d --name="tomcat-run" -v "/home/eclairiuss/Documents/stage/training-java/target/war:/usr/local/tomcat/webapps" -p 127.0.0.1:8080:8080 -p 127.0.0.1:8000:8000 eclairiuss/tomcat
+docker run --network my_network -d --name="mysql-run" -e MYSQL_PASSWORD=root -p 127.0.0.1:3306:3306 eclairiuss/mysql
